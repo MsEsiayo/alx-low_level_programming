a342429d8e84e23e36bd61ba3fa6b0a312512ea6
MsEsiayo <esiayoajisola@yahoo.com>
2022-12-10 12:49:11 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Decription: Prints a text using the printf function
+*
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
