4158a469fe3f2bb47bef43a19d1ef168b41f6bed
MsEsiayo <esiayoajisola@yahoo.com>
2022-12-10 13:13:28 -0800

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Print the size of various data types
+*
+* Description: Prints the various data types using the printf function
+*
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long %d byte(s)\n", sizeof(lli));
+	printf("Size of a float %d byte(s)\n", sizeof(f));
+	return (0);
+}
