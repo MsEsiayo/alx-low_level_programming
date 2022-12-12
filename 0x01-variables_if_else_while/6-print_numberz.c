6e880a8d0330ac63cc97f979b4baa2af769db2a7
MsEsiayo <esiayoajisola@yahoo.com>
2022-12-12 15:12:32 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - A program that prints the numbers in base ten
+*
+* Description: Using a for loop to print in digits
+*
+* Return: 0 whenever the program is sucessful
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
