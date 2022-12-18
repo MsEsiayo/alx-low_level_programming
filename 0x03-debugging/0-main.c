4ce15309f997d6894759f409a77bbb086b21e563
MsEsiayo <esiayoajisola@yahoo.com>
2022-12-18 13:44:07 -0800

    Mult mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+/**
+* main - tests function that prints if integer is positive or negative
+*
+* Description: Program to check if a digit is positive or not
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
