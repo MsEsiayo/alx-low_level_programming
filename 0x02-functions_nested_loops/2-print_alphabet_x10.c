384e09520d7f829771f6f3f02622acd767a9ba66
MsEsiayo <esiayoajisola@yahoo.com>
2022-12-18 03:56:00 -0800

    alpha alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print alphabet 10 times
+*
+* Description: program using functions
+*
+* Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char alpha = 'a';
+
+	while (i < 10)
+	{
+		alpha = 'a';
+
+		while (alpha <= 'z')
+		{											_putchar(alpha);
+			alpha++;																		}
+		_putchar('\n');
+		i++;
+	}
+}
