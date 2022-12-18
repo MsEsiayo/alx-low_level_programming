7a2c0acbe785fcd28353d1b01c929fb8c4bb451d
MsEsiayo <esiayoajisola@yahoo.com>
2022-12-18 03:21:29 -0800

    Letters game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main- Printing the alpha
+*
+* Description: printing the alpha
+*
+* Return: 0
+*/
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		{
+
+		_putchar(c);
+		}
+	_putchar('\n');
+}
