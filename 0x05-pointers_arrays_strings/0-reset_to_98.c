47a3b322404f22ce0681e3835785627eb2b2ca87
MsEsiayo <esiayoajisola@yahoo.com>
2022-12-20 00:40:33 -0800

    98 battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+* reset_to_98 - check the code for Holberton School students.
+* @n: declaration of n and paramters for the function reset_to_98
+*
+* Return: Always 0.
+*/
