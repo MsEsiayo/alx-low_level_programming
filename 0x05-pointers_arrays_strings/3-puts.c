ec4b4c72f46b52b95f11ea47159e2ca252cf6abd
MsEsiayo <117748859+MsEsiayo@users.noreply.github.com>
2022-12-20 10:19:23 +0100

    Fear of computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts - function that prints a string, followed by a new line
- * @str: declaration of *str and paramters for the function _puts
+ * _puts - function that prints a string
+ * @str: declaration of str 
  * Return: Always 0.
  */
 void _puts(char *str)
 {
-	int c;
+	int i;
 
-	for (c = 0; str[c] != 0; c++)
+	for (i = 0; str[i] != 0; i++)
 	{
-		_putchar(str[c]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
