6f36ed9cf69a8c5ce7b5fe034e2d2004d497870a
MsEsiayo <117748859+MsEsiayo@users.noreply.github.com>
2022-12-20 10:02:40 +0100

    Long report

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,2 +1,18 @@
 #include "main.h"
 
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+for (c = 0; s[c] != 0; c++)
+{
+}
+return (c);
+}
+
